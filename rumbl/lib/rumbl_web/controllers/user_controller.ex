fcfceac71aae53f6a7b2104d1163a5601e281ac9
Sergey <mk-serg@yandex.ru>
2019-03-22 14:04:55 +0300

    Page with information about individual User (dummy data)
    route user/:id
    controller, view, templates

--- rumbl/lib/rumbl_web/controllers/user_controller.ex
+++ rumbl/lib/rumbl_web/controllers/user_controller.ex
@@ -2,9 +2,15 @@ defmodule RumblWeb.UserController do
   use RumblWeb, :controller
   alias Rumbl.Account.User
   alias Rumbl.Account
+
   def index(conn, _params) do
     users = Account.all(User)
     render(conn, "index.html", users: users)
+  end
+
+  def show(conn, %{"id" => id}) do
+    user = Account.get(User, id)
+    render(conn, "show.html", user: user)
   end
 end
   
